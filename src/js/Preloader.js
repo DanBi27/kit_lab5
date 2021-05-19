a5b7912209756717231c9f89b2369be63a860d06
DanBi27 <70751426+DanBi27@users.noreply.github.com>
2021-05-20 01:00:53 +0300

    Initial commit

---
+++ src/js/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Preloader extends React.Component {
+  render() {
+    return (
+      <div id="preloader">
+        <img src="https://acegif.com/wp-content/uploads/loading-9.gif" alt="" />
+      </div>
+    );
+  }
+}
---
+++ src/js/UserAvatar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UserAvatar(props) {
+  return <img src={props.src} width={100} height={100} alt="usrAvatar" />;
+}
